2efe23def63a37b6f78e1928550c77e6e64c7fb2
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-18 19:23:04 +0200

    Updates

---
+++ precache-manifest.b374effa5b59f61d5355f11870241270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fae6591c2a8b3136f382f6117319d49",
+    "url": "/Piotrn-87/Weather-Application/index.html"
+  },
+  {
+    "revision": "8014a6ce2a5e553933d0",
+    "url": "/Piotrn-87/Weather-Application/static/css/main.3f9ce68a.chunk.css"
+  },
+  {
+    "revision": "27e112012732af5bed4a",
+    "url": "/Piotrn-87/Weather-Application/static/js/2.5284389e.chunk.js"
+  },
+  {
+    "revision": "8014a6ce2a5e553933d0",
+    "url": "/Piotrn-87/Weather-Application/static/js/main.870de551.chunk.js"
+  },
+  {
+    "revision": "ac1c7d27a0a4b9e40611",
+    "url": "/Piotrn-87/Weather-Application/static/js/runtime-main.6f38a9a4.js"
+  },
+  {
+    "revision": "0b0fc0e701662156ac42bc5e4e0eabce",
+    "url": "/Piotrn-87/Weather-Application/static/media/weather.0b0fc0e7.jpg"
+  }
+]);
