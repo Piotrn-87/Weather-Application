179ba378f0dbeecf6cbc2293a176d2a5107e190e
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-18 16:05:56 +0200

    Feat: Styling Result Component

--- src/components/Result.js
+++ src/components/Result.js
@@ -22,34 +22,37 @@ const Result = props => {
   if (!error && city) {
     const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
     const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
+    const firstLetterUppercase = e => {
+      return e.charAt(0).toUpperCase() + e.slice(1);
+    };
     content = (
-      <div>
-        <h1 className="Date"> {date} </h1>
-        <h2 className="Localization">
+      <div className="Content">
+        <h4 className="Content__description">Updated:</h4>
+        <h1 className="Content__date"> {date} </h1>
+        <h2 className="Content__localization">
           {" "}
-          <FontAwesomeIcon
-            className="Localization__icon"
-            icon={faMapMarkerAlt}
-          />
+          <FontAwesomeIcon className="Content__icon" icon={faMapMarkerAlt} />
           {city}, {country}{" "}
         </h2>
-        <h1 className="Temp">{temp} &#176;C</h1>
-        <h4> Minimalna temperatura: {minTemp} &#176;C</h4>
-        <h4> Maksymalna temperatura: {maxTemp} &#176;C</h4>
-        <h4>Aktualne ciśnienie: {pressure} hpa</h4>
-        <h4>Siła wiatru: {wind} m/s</h4>
-        <h4>Wschód słońca: {sunriseTime}</h4>
-        <h4>Zachód słońca: {sunsetTime}</h4>
+        <div className="ContentTemp">
+          <h1 className="ContentTemp__temperature">{temp} &#176;C</h1>
+          <span className="ContentTemp__min-max">
+            <h4> Min temp: {minTemp} &#176;C</h4>
+            <h4> Max temp: {maxTemp} &#176;C</h4>
+          </span>
+        </div>
+        <h2>Pressure: {pressure} hpa</h2>
+        <h2>Wind: {wind} m/s</h2>
+        <h2>Sunrise: {sunriseTime}</h2>
+        <h2>Sunset: {sunsetTime}</h2>
       </div>
     );
   }
 
   return (
     <React.Fragment>
-      <div className="Result">
-        {" "}
-        {error ? `Brak w bazie: ${city}` : content}{" "}
-      </div>{" "}
+      {" "}
+      {error ? `Brak w bazie: ${city}` : content}{" "}
     </React.Fragment>
   );
 };
