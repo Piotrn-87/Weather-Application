eb57cf3ec434326dcb1f9715c7848dd0d31b2bf6
Piotr Napieralski <piotrn-87@o2.pl>
2019-11-11 09:25:44 +0100

    Updates

--- precache-manifest.ff16b35eca864833eee447ad804d36fe.js
+++ precache-manifest.ff16b35eca864833eee447ad804d36fe.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df7fe83138a8ce2bbcb6b34756880224",
+    "revision": "a5f79689e5dfd33dd3fa805ae7098429",
     "url": "/Weather-Application/index.html"
   },
   {
-    "revision": "2a5993e6314696eff075",
+    "revision": "61cf0abd0d0ae3ce98e3",
     "url": "/Weather-Application/static/css/main.bcea9d6f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Weather-Application/static/js/2.5284389e.chunk.js"
   },
   {
-    "revision": "2a5993e6314696eff075",
-    "url": "/Weather-Application/static/js/main.da4a921f.chunk.js"
+    "revision": "61cf0abd0d0ae3ce98e3",
+    "url": "/Weather-Application/static/js/main.e108c4d7.chunk.js"
   },
   {
     "revision": "d0f7dbfd6bde7897e24b",
